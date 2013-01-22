15d74952b15ad3262fe7f0b917a5e676e34ff83f
Dirk Lüth <dlskynet@gmx.de>
2013-01-22 12:59:12 +0100

    [UPDATE] release 1.0.4
    - added jQuery plugin manifest

--- packages/qoopido.shrinkimage.js
+++ packages/qoopido.shrinkimage.js
@@ -2,7 +2,7 @@
 * Qoopido jQuery Plugin "shrinkimage"
 *
 * Source:  Qoopido JS
-* Version: 1.0.3
+* Version: 1.0.4
 * Date:    2013-01-22
 * Author:  Dirk Lüth <info@qoopido.com>
 * Website: https://github.com/dlueth/Qoopido-JS
