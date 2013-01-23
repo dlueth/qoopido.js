a11d511953db648dc0b424ffdd06de166dd326cc
Dirk Lüth <dlskynet@gmx.de>
2013-01-24 00:43:24 +0100

    [UPDATE] changed repository name

--- packages/qoopido.shrinkimage.js
+++ packages/qoopido.shrinkimage.js
@@ -2,7 +2,7 @@
 * Qoopido jQuery Plugin "shrinkimage"
 *
 * Source:  Qoopido JS
-* Version: 1.0.10
+* Version: 1.1.0
 * Date:    2013-01-24
 * Author:  Dirk Lüth <info@qoopido.com>
 * Website: https://github.com/dlueth/Qoopido-JS
