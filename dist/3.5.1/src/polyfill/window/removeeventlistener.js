24346d7e9f5ecb8b9a739272557c025c685c9f8f
Dirk Lüth <info@qoopido.com>
2014-09-10 12:34:55 +0200

    [UPDATE] hook/css
    - fix usage of wrong property

---
+++ dist/3.5.1/src/polyfill/window/removeeventlistener.js
@@ -0,0 +1,38 @@
+/*!
+* Qoopido.js library
+*
+* version: 3.5.1
+* date:    2014-8-10
+* author:  Dirk Lueth <info@qoopido.com>
+* website: https://github.com/dlueth/qoopido.js
+*
+* Copyright (c) 2014 Dirk Lueth
+*
+* Dual licensed under the MIT and GPL licenses.
+* - http://www.opensource.org/licenses/mit-license.php
+* - http://www.gnu.org/copyleft/gpl.html
+*/
+(function(definition) {
+    var dependencies = [];
+    if (!Array.prototype.indexOf) {
+        dependencies.push("../array/indexof");
+    }
+    window.qoopido.register("polyfill/window/removeeventlistener", definition, dependencies);
+})(function(modules, shared, namespace, navigator, window, document, undefined) {
+    "use strict";
+    if (!window.removeEventListener) {
+        window.removeEventListener = Window.prototype.removeEventListener = HTMLDocument.prototype.removeEventListener = Element.prototype.removeEventListener = function removeEventListener(type, listener) {
+            var element = this;
+            if (element._events && element._events[type] && element._events[type].list) {
+                var index = element._events[type].list.indexOf(listener);
+                if (index > -1) {
+                    element._events[type].list.splice(index, 1);
+                    if (!element._events[type].list.length) {
+                        element.detachEvent && element.detachEvent("on" + type, element._events[type]);
+                    }
+                }
+            }
+        };
+    }
+    return window.removeEventListener;
+});
---
+++ dist/latest/src/support/element/video.js
@@ -0,0 +1,25 @@
+/*!
+* Qoopido.js library
+*
+* version: 3.5.1
+* date:    2014-8-10
+* author:  Dirk Lueth <info@qoopido.com>
+* website: https://github.com/dlueth/qoopido.js
+*
+* Copyright (c) 2014 Dirk Lueth
+*
+* Dual licensed under the MIT and GPL licenses.
+* - http://www.opensource.org/licenses/mit-license.php
+* - http://www.gnu.org/copyleft/gpl.html
+*/
+(function(definition) {
+    window.qoopido.register("support/element/video", definition, [ "../../support" ]);
+})(function(modules, shared, namespace, navigator, window, document, undefined) {
+    "use strict";
+    var support = modules["support"];
+    return support.addTest("/element/video", function(deferred) {
+        var sample = support.pool ? support.pool.obtain("video") : document.createElement("video");
+        sample.canPlayType ? deferred.resolve() : deferred.reject();
+        sample.dispose && sample.dispose();
+    });
+});
