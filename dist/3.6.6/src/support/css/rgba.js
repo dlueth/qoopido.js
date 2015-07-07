2e5435ad5422466cdb79b3c7534ba4e12671ac47
Dirk Lüth <info@qoopido.com>
2015-07-07 10:45:23 +0200

    [RELEASE] 3.6.6

---
+++ dist/3.6.6/src/support/css/rgba.js
@@ -0,0 +1,28 @@
+/*!
+* Qoopido.js library
+*
+* version: 3.6.6
+* date:    2015-7-7
+* author:  Dirk Lueth <info@qoopido.com>
+* website: https://github.com/dlueth/qoopido.js
+*
+* Copyright (c) 2015 Dirk Lueth
+*
+* Dual licensed under the MIT and GPL licenses.
+* - http://www.opensource.org/licenses/mit-license.php
+* - http://www.gnu.org/copyleft/gpl.html
+*/
+(function(definition) {
+    window.qoopido.register("support/css/rgba", definition, [ "../../support" ]);
+})(function(modules, shared, namespace, navigator, window, document, undefined) {
+    "use strict";
+    var support = modules["support"];
+    return support.addTest("/css/rgba", function(deferred) {
+        var sample = support.pool ? support.pool.obtain("div") : document.createElement("div");
+        try {
+            sample.style.backgroundColor = "rgba(0,0,0,.5)";
+        } catch (exception) {}
+        /rgba/.test(sample.style.backgroundColor) ? deferred.resolve() : deferred.reject();
+        sample.dispose && sample.dispose();
+    });
+});
