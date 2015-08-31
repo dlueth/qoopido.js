2c63783dd6d877b5e0baea39dd6412b411d93bc1
Dirk Lüth <info@qoopido.com>
2015-08-31 13:39:09 +0200

    [UPDATE] demo code

--- demo/main.js
+++ demo/main.js
@@ -3,7 +3,8 @@
 
 	demand
 		.configure({
-			base: 'https://rawgit.com/dlueth/qoopido.js/release/4.0.0/demo',
+			version: '1.0.0',
+			base:    'https://rawgit.com/dlueth/qoopido.js/release/4.0.0/demo',
 			pattern: {
 				'/qoopido': 'https://rawgit.com/dlueth/qoopido.js/release/4.0.0/dist/latest/min'
 			}
@@ -12,17 +13,17 @@
 	demand('app/test', '/qoopido/component/iterator')
 		.then(
 			function(appTest, qoopidoComponentIterator) {
-				console.log('=> success', appTest, qoopidoComponentIterator);
+				console.log('[success]', appTest, qoopidoComponentIterator);
 
 				new qoopidoComponentIterator();
 			},
 			function(error) {
-				console.log('=> error', error);
+				console.log('[error]', error);
 			}
 		);
 
 	function definition(appTest, qoopidoBase) {
-		console.log('/app/main', appTest, qoopidoBase);
+		console.log('[success] /app/main =>', appTest, qoopidoBase);
 
 		return function appMain() {
 
@@ -30,4 +31,14 @@
 	}
 
 	provide('/app/main', definition).when('test', '/qoopido/base');
+
+	demand('text/css!default')
+		.then(
+			function(cssDefault) {
+				console.log('[success]', cssDefault);
+			},
+			function(error) {
+				console.log('[error]', error);
+			}
+		);
 }());
