5a230da2ea939a0e9fbf907a106e78253b4a81a2
Dirk Lüth <info@qoopido.com>
2015-09-01 16:21:26 +0200

    [UPDATE] demo code

--- demo/main.js
+++ demo/main.js
@@ -1,17 +1,16 @@
-;(function() {
+;(function(global, demand, provide) {
 	'use strict';
 
-	demand
-		.configure({
-			version: '1.0.0',
-			base:    'https://cdn.rawgit.com/dlueth/qoopido.js/release/4.0.0/demo',
-			pattern: {
-				'/qoopido': 'https://cdn.rawgit.com/dlueth/qoopido.js/release/4.0.0/dist/latest/min',
-			}
-		});
+	function definition() {
+		demand
+			.configure({
+				pattern: {
+					'/qoopido': '/src', // https://rawgit.com/dlueth/qoopido.js/release/4.0.0/dist/latest/min'
+				}
+			});
 
-	demand('app/test', '/qoopido/component/iterator')
-		.then(
+		demand('app/test', '/qoopido/component/iterator')
+			.then(
 			function(appTest, qoopidoComponentIterator) {
 				console.log('[success]', appTest, qoopidoComponentIterator);
 
@@ -22,19 +21,19 @@
 			}
 		);
 
-	function definition(appTest, qoopidoBase) {
-		console.log('[success] /app/main =>', appTest, qoopidoBase);
+		function definition(appTest, qoopidoBase) {
+			console.log('[success] /app/main =>', appTest, qoopidoBase);
 
-		return function appMain() {
+			return function appMain() {
 
+			}
 		}
-	}
 
-	provide('/app/main', definition)
-		.when('test', '/qoopido/base');
+		provide('/app/main', definition)
+			.when('test', '/qoopido/base');
 
-	demand('text/css!default')
-		.then(
+		demand('text/css!default')
+			.then(
 			function(cssDefault) {
 				console.log('[success]', cssDefault);
 
@@ -44,4 +43,7 @@
 				console.log('[error]', error);
 			}
 		);
-}());
+	}
+
+	provide(definition);
+}(this, demand, provide));
